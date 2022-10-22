7d371c7111f475ceb64d81fd0919013769337e1d
Matt Murphy <matt@murphysys.com>
2022-10-21 22:16:47 -0700

    typo

--- lab4.asm
+++ lab4.asm
@@ -8,7 +8,7 @@
   LDI 0x2 B     ; limit in register B
 
 loop:
-  CMPJ B A end    ; continue looping while B >= A
+  CMPJ B A 2    ; continue looping while B >= A
   JMP end       ; escape loop if CMPJ failed
   INC A A       ; increment A
   JMP loop
